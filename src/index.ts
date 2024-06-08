cf1700d738fb92f9878a4ca928db89a64c0ab84e
Pedro César <pedroocesarmf@gmail.com>
2024-06-08 20:00:47 -0300

    chore: configuração inicial do projeto

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express, { Express, Request, Response} from 'express';
+
+
+const app: Express = express();
+const port = 5000;
+
+app.listen(port, () => {
+    console.log(`Server aberto na porta: ${port}`);
+})
