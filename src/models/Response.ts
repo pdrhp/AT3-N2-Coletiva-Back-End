dd00894c0998d486e1b16dc01f982cd608390a34
pdrhp <pedrohsp2004@gmail.com>
2024-06-11 20:46:15 -0300

    feat: created Response class to use a Response Pattern in the api

---
+++ src/models/Response.ts
@@ -0,0 +1,14 @@
+
+class Response<T>{
+    status_code: number;
+    message: string;
+    data?: T;
+
+    constructor(status_code: number, message: string, data?: T) {
+        this.status_code = status_code;
+        this.message = message;
+        this.data = data;
+    }
+}
+
+export default Response;
