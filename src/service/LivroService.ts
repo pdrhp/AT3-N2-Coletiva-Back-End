f7a3e61717fe69f1eb42b31a7510d469bafa5dbe
pdrhp <pedrohsp2004@gmail.com>
2024-06-15 09:58:55 -0300

    feat: created the method in service to add a livro to the database

--- src/service/LivroService.ts
+++ src/service/LivroService.ts
@@ -1,17 +1,22 @@
 import Livro from "../models/Livro";
 import Response from "../models/Response";
-import { getAllLivros } from "../repository/LivroRepository";
+import LivroRepository from "../repository/LivroRepository";
 
+
+const livroRepository = new LivroRepository();
 
 const getLivros = async (): Promise<Response<Livro[]>> => {
-    const livros: Livro[] = await getAllLivros();
-
+    const livros: Livro[] = await livroRepository.getAll();
     if(livros.length === 0){
         return new Response(404, "Nenhum livro encontrado");
     }
-
     return new Response(200, "Livros encontrados", livros);
 }
 
+export const addLivro = async (livro: Livro): Promise<Response<Livro>> => {
+    const result = await livroRepository.create(livro);
+    const livroCriado: Livro = await livroRepository.getById(result.lastID);
+    return new Response(201, "Livro adicionado com sucesso", livroCriado);
+}
 
 
