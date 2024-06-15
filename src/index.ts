fabf098691b18a89558fe0ccd87c5d17ce3048b3
Pedro César <pedroocesarmf@gmail.com>
2024-06-15 19:02:55 -0300

    fix: added allow all cors rule

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express, { Express } from 'express';
 import LivroController from './controllers/LivroController';
 import { initDatabase } from './data/database';
-
+import cors from 'cors';
 
 const app: Express = express();
 const port = 5000;
+
+app.use(cors());
 
 app.use(express.json());
 
