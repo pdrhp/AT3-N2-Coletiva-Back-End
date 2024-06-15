e8a9a1206987c051bc7568e8b4b31d80312a546c
pdrhp <pedrohsp2004@gmail.com>
2024-06-15 13:32:00 -0300

    fix: fixing the getByName method in repository

--- src/repository/LivroRepository.ts
+++ src/repository/LivroRepository.ts
@@ -10,20 +10,18 @@ class LivroRepository implements IRepository<Livro> {
     }
 
     getById = async (id: number) => {
-        const result: Livro = await getQuery(`SELECT id, titulo, autor, genero, quantidade FROM Livros WHERE id = ${id}`)
+        const result: Livro = await getQuery(`SELECT id, titulo, autor, genero, quantidade FROM Livros WHERE id = ?`, [id]);
         return result;
     }
 
     getByName = async (titulo: string) => {
         titulo = titulo.toLowerCase();
-        const result: Livro = await getQuery(`SELECT id, titulo, autor, genero, quantidade FROM Livros WHERE titulo_normalized LIKE '${titulo}'`)
+        const result: Livro[] = await getAllQuery(`SELECT id, titulo, autor, genero, quantidade FROM Livros WHERE normalized_titulo LIKE ?`, [`%${titulo}%`])
         return result;
     }
 
     create = async (livro: Livro) => {
-
         const normalized_titulo = livro.titulo.toLowerCase();
-
         const query = `INSERT INTO Livros (titulo, normalized_titulo, autor, genero, quantidade) VALUES (?, ?, ?, ?, ?)`
         const parameters = [livro.titulo, normalized_titulo, livro.autor, livro.genero, livro.quantidade]
         const result = await execQuery(query, parameters);
