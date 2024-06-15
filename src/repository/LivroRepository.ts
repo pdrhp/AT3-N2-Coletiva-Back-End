1cf841f2c7cf894e2c222a6b251d366cd91fe4e1
pdrhp <pedrohsp2004@gmail.com>
2024-06-15 09:39:08 -0300

    feat: added the method to add a livro to the database in repository

--- src/repository/LivroRepository.ts
+++ src/repository/LivroRepository.ts
@@ -1,4 +1,4 @@
-import { runGetAllQuery, runGetQuery } from "../data/database";
+import { insertQuery, runGetAllQuery, runGetQuery } from "../data/database";
 import Livro from "../models/Livro";
 
 export const getAllLivros = async () => {
@@ -15,4 +15,18 @@ export const getLivroByName = async (titulo: string) => {
     const result: Livro = await runGetQuery(`SELECT * FROM Livros WHERE titulo = '${titulo}'`)
     return result;
 }
+
+export const AddLivro = async (livro: Livro) => {
+
+    const query = `INSERT INTO Livros (titulo, autor, genero, quantidade) VALUES (?, ?, ?, ?)`
+    const parameters = [livro.titulo, livro.autor, livro.genero, livro.quantidade]
+
+    const result = await insertQuery(query, parameters);
+
+    return result;
+}
+
+
+
+
 
